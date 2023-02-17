d718e3b094f64ef7cc335ffc5b901f3f728fdec3
Jasmeet Kaur <jasmeetkaur9@github.io>
2023-02-16 21:50:21 -0600

    code clean up

--- get_results.py
+++ get_results.py
@@ -31,7 +31,8 @@ if __name__ == "__main__":
         e_dist, e_times = get_execution_distributions(num_of_plans, actions_per_plan, max_execution_time=3)
         # print(dist)
         # print(e_dist)
-        env = MetaWorldEnv(num_of_plans, actions_per_plan, deadline, actions, max_planning_time, False)
+        env = MetaWorldEnv(num_of_plans, actions_per_plan, deadline, actions, max_planning_time, False,
+                           dist,planning_times,e_dist,e_times)
         mw = MetaReasoningWorld(env,False)
 
         # Print the number of successful terminal states, total terminal states, total states
@@ -82,45 +83,43 @@ if __name__ == "__main__":
         list_k_sd = []
 
         # k is the number of rollout iterations
-        for k in [1000]:
-            num_of_trials = 10
+        for k in [8000]:
+            num_of_trials = 100
             total_reward = 0.0
             s = []
             for i in range(0, num_of_trials):
                 st_time = time.time()
-                curr_state = 0
+                curr_id = 0
                 pp = []
-                reward = 0
+                reward = 0.0
                 state_path = []
-                test = 0
-                # Till the end of episode
                 while True:
-                    reward = reward + env.reward_model[curr_state]
-                    root_node = nd.Node(curr_state)
-
+                    reward = reward + env.reward_model[curr_id]
+                    root_node = nd.Node(curr_id)
                     mcts = MCTS(root_node, env, False)
                     best_action = mcts.Run(k)
-
                     pp.append(best_action)
-                    state_path.append(env.get_state_from_id(curr_state))
-                    next_state, _ = env.step_next_state(curr_state, best_action)
-                    curr_state = next_state
-                    if env.done(curr_state):
-                        reward = reward + env.reward_model[curr_state]
+                    state_path.append(env.get_state_from_id(curr_id))
+                    res, _, _ = env.step(curr_id, best_action)
+                    list1 = list(res.keys())
+                    list2 = list(res.values())
+                    curr_id = (random.choices(list1, weights=list2, k=1))[0]
+                    if env.done(curr_id):
+                        reward = reward + env.reward_model[curr_id]
                         break
-                    test = test + 1
-                    pp.pop()
-                    total_reward = total_reward + reward
-                    s.append(reward)
-                print(np.mean(s))
-                list_k.append(np.mean(s))
-                list_k_sd.append(np.std(s))
-                t1 = time.time() - st_time
-                # print(st.t.interval(confidence=0.95, df=len(s) - 1, loc=np.mean(s), scale=st.sem(s)))
-            # for i in range(0, len(list_k)):
-            #     print(list_k[i])
-            # for i in range(0, len(list_k)):
-            #     print(list_k_sd[i])
-        avg = []
-        moving_avg = []
-        std = []
+                pp.pop()
+                total_reward = total_reward + reward
+                s.append(reward)
+            list_k.append(np.mean(s))
+            list_k_sd.append(np.std(s))
+            t1 = time.time() - st_time
+            # print(st.t.interval(confidence=0.95, df=len(s) - 1, loc=np.mean(s), scale=st.sem(s)))
+            # ctime[k][sample_num] = t1
+            # cost_values[k][sample_num] = np.mean(s)
+        for i in range(0, len(list_k)):
+            print(list_k[i])
+        for i in range(0, len(list_k)):
+            print(list_k_sd[i])
+    avg = []
+    moving_avg = []
+    std = []
